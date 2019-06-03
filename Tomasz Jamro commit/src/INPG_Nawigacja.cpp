08e9632a8ad670881020a1b576f2288c3906fe4f
t17jamro <49689803+t17jamro@users.noreply.github.com>
2019-06-03 22:35:18 +0200

    Update INPG_Nawigacja.cpp

--- Tomasz Jamro commit/src/INPG_Nawigacja.cpp
+++ Tomasz Jamro commit/src/INPG_Nawigacja.cpp
@@ -17,7 +17,7 @@
             oss.clear();
         }
     }
-    // Ważne !!! - przydatny warunek do odczytywania listy sasiednich punktów to !isspace(*it) - wykrywa brak spacji
+    // WaĹĽne !!! - przydatny warunek do odczytywania listy sasiednich punktĂłw to !isspace(*it) - wykrywa brak spacji
     int id = std::stoi( pointstring[0] );
     int x = std::stoi( pointstring[1] );
     int y = std::stoi( pointstring[2] );
@@ -39,6 +39,7 @@
     list.push_back(temp_var);
     oss.str("");
     oss.clear();
+    pointstring = {};
     id_= id;
     x_=x;
     y_=y;
@@ -47,7 +48,7 @@
 }
 
 void PointList::Import_Map() {
-    std::ostringstream oss;
+    PointList_ = {};
     std::string linia;
     std::fstream plik;
     plik.open( "C:\\Users\\To_Ja\\Desktop\\Inform\\mdig-agh-air_inf1_lab_cpp_exercises-8565224dab68\\solutions\\INPG_Nawigacja\\BazaDanych.txt", std::ios::in);
@@ -56,8 +57,39 @@ void PointList::Import_Map() {
         while(!plik.eof())
         {
             getline(plik, linia);
-            PointList_.push_back(Point(linia)); //wyświetlenie linii
+            auto a = Point(linia);
+            PointList_.push_back(a); //tworzenie punktu za pomocÄ… lini
         }
         plik.close();
     } else std::cout << "Dostep do pliku zostal zabroniony!" << std::endl;
+}
+
+std::size_t PointList::find(int x) const {
+    for (std::size_t posit =0 ; posit<PointList_.size(); posit++)
+    {
+        if (PointList_[posit].getid() == x) return posit;
+    }
+    return 0; // Do zmiany - dodadanie wyjÄ…tku gdy nie znajdzie siÄ™ punktu WAĹ»NE
+}
+
+int find_lack_of_id() {
+    std::string linia;
+    std::fstream plik;
+    int number = 0;
+    int i = 0;
+    plik.open( "C:\\Users\\To_Ja\\Desktop\\Inform\\mdig-agh-air_inf1_lab_cpp_exercises-8565224dab68\\solutions\\INPG_Nawigacja\\BazaDanych.txt", std::ios::in);
+    if( plik.good())
+    {
+        while(!plik.eof()) {
+            getline(plik, linia);
+            number = 0;
+            auto it = linia.begin();
+            if(*it == ""){
+                return i;
+            }
+            i++;
+        }
+        plik.close();
+    } else std::cout << "Dostep do pliku zostal zabroniony!" << std::endl;
+    return 0; // Do zmiany - dodadanie wyjÄ…tku gdy baza jest poprawna
 }
