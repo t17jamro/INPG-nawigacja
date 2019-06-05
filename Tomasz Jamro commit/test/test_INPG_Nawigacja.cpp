5fbd6887f95bdab5ff83b58cc7fcb6fec4236f17
t17jamro <49689803+t17jamro@users.noreply.github.com>
2019-06-05 22:44:41 +0200

    Update test_INPG_Nawigacja.cpp

--- Tomasz Jamro commit/test/test_INPG_Nawigacja.cpp
+++ Tomasz Jamro commit/test/test_INPG_Nawigacja.cpp
@@ -1,9 +1,10 @@
 #include "gtest/gtest.h"
 #include <math.h>
 #include "INPG_Nawigacja.hpp"
+// UWAGA! Testy należy wykonywać z plikiem tekstowym BazaTestowa.txt, korzystanie z oficjalnej bazy danych nie zadziała
 
-
-TEST(PointCreateTest, NormalConstructor) {
+TEST(PointCreateTest, NormalConstructor) {   
+    //Test na tworzenie obiektu Punkt - podanie dokładnych danych do każdego pola
     Point testpoint(4,34,54,"Nazwa",std::vector<int>{55,6,8});
     EXPECT_EQ(testpoint.getlist().size(),3U);
     EXPECT_EQ(testpoint.getlist()[2],8);
@@ -13,7 +14,8 @@ TEST(PointCreateTest, NormalConstructor) {
     EXPECT_EQ(testpoint.getname(),"Nazwa");
 }
 
-TEST(PointCreateTest, StringConstructor){
+TEST(PointCreateTest, StringConstructor){ 
+    //test na tworzeniu obiektu Punkt z pojedyńczej lini tekstu, w kodowaniu odpowiednim dla bazy danych
     Point testpoint("5#35#55#Nazwa2#64 63 81#");
     EXPECT_EQ(testpoint.getlist().size(),3U);
     EXPECT_EQ(testpoint.getlist()[2],81);
@@ -24,35 +26,32 @@ TEST(PointCreateTest, StringConstructor){
 
 }
 
-TEST(PointCreateTest, ImportMap) {
-    Point x(4,34,54,"Nazwa",std::vector<int>{55,6,8});
+TEST(MapCreateTest, ImportMapFunction) { 
+    //test na tworzeniu Obiektu Listy Punktów z bazy danych
+    //Powinien być tworzony przez przypadkowy punkt a następnie odrazu y poprzez importowanie danych
+    Point x(10,34,54,"Nazwa",std::vector<int>{55,6,8});
     PointList test(x);
     Point testpoint = test[0];
     EXPECT_EQ(testpoint.getlist().size(),3U);
     EXPECT_EQ(testpoint.getlist()[2],8);
-    EXPECT_EQ(testpoint.getid(),4);
+    EXPECT_EQ(testpoint.getid(),10);
     EXPECT_EQ(testpoint.getx(),34);
     EXPECT_EQ(testpoint.gety(),54);
     EXPECT_EQ(testpoint.getname(),"Nazwa");
     test.Import_Map(); // WAŻNE, zmien ścieżkę dostępu do twojego $REPO$\INPG_Nawigacja\BazaDanych.txt w pliku źródłowym INPG_Nawigacja.cpp
-    Point testpoint2 = test[1];
-    EXPECT_EQ(testpoint2.getlist().size(),3U);
-    EXPECT_EQ(testpoint2.getlist()[2],36);
-    EXPECT_EQ(testpoint2.getid(),456);
-    EXPECT_EQ(testpoint2.getx(),234);
-    EXPECT_EQ(testpoint2.gety(),643);
-    EXPECT_EQ(testpoint2.getname(),"Nazwa1");
-    Point testpoint3 = test[2];
-    EXPECT_EQ(testpoint3.getlist().size(),3U);
-    EXPECT_EQ(testpoint3.getlist()[2],36);
-    EXPECT_EQ(testpoint3.getid(),456);
-    EXPECT_EQ(testpoint3.getx(),234);
-    EXPECT_EQ(testpoint3.gety(),643);
-    EXPECT_EQ(testpoint3.getname(),"Nazwa2");
+    Point testpoint2 = test[0];
+    EXPECT_EQ(testpoint2.getlist().size(),2U);
+    EXPECT_EQ(testpoint2.getlist()[1],3);
+    EXPECT_EQ(testpoint2.getid(),1);
+    EXPECT_EQ(testpoint2.getx(),0);
+    EXPECT_EQ(testpoint2.gety(),0);
+    EXPECT_EQ(testpoint2.getname(),"PunktA");
 
 }
 
-TEST(PointCreateTest, FindPointMapFunction) {
+
+TEST(MapCreateTest, FindPointMapFunction) {
+    //test na szukanie pozycji pojedyńczego punktu w środku obiektu Listy punktów poprzez id
     Point x(4,34,54,"Nazwa",std::vector<int>{55,6,8});
     PointList test(x);
     std::size_t xpos = test.find(4);
@@ -62,8 +61,35 @@ TEST(PointCreateTest, FindPointMapFunction) {
     EXPECT_EQ(test[t].gety(),x.gety());
 }
 
-TEST(PointCreateTest, DistanceFunction){
+TEST(RoadCreateTest, DistanceFunction){
+    //test na obliczanie długości pomiędzy dwoma punktami poprzez funkcje
     Point x(4,0,0,"Nazwa",std::vector<int>{55,6,8});
     Point t(4,3,4,"Nazwa",std::vector<int>{55,6,8});
     EXPECT_EQ(distance(x,t),sqrt(25));
+}
+
+TEST(RoadCreateTest,DistanceTotal){
+    //test na tworzenie obiektu drogi i obliczanie jego długości
+    std::vector<Point> q = {};
+    Point x(4,0,0,"Nazwa",std::vector<int>{55,6,8});
+    Point t(4,3,4,"Nazwa",std::vector<int>{55,6,8});
+    Point f(4,3,9,"Nazwa",std::vector<int>{55,6,8});
+    Point tef(4,-2,9,"Nazwa",std::vector<int>{55,6,8});
+    q.push_back(x);
+    q.push_back(t);
+    q.push_back(f);
+    q.push_back(tef);
+    PointRoad road1(q);
+    EXPECT_EQ(road1.distance_calculate(),15);
+}
+
+TEST(RoadCreateTest,DistanceTotalRoad){
+    //Test na poprawne tworzenie drogi, i obliczanie jej odległości
+    Point x(4,34,54,"Nazwa",std::vector<int>{55,6,8});
+    PointList test(x);
+    Point testpoint = test[0];
+    test.Import_Map();
+    std::vector<Point> radse = FindRoad(3,6,test);
+    PointRoad q(radse);
+    EXPECT_EQ(q.distance_calculate(),15);
 }
